8f7238eb06bccc59805a29270ff08c5d706204c4
zuzakowalska <zuzanna@kowalska.pro>
2017-08-03 16:27:03 +0200

    name and mail validation done

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,27 @@
 'use strict';
 
 function checkTextField() {
-    
+
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var nameRequired = document.getElementById("name-required");
-    
-    
+    var emailRequired = document.getElementById("email-required");
+
     if (name.length == 0) {
         event.preventDefault();
         console.log("no name");
         nameRequired.style.opacity = "1";
     } else {
+        nameRequired.style.opacity = "0";
         console.log(name);
+    }
+
+    if (email.length == 0) {
+        event.preventDefault();
+        console.log("no email");
+        emailRequired.style.opacity = "1";
+    } else {
+        nameRequired.style.opacity = "0";
+        console.log(email);
     }
 }
