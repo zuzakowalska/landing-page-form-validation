b9ffb94b4a7e95cc2f1d61d19ee2521513de33a4
zuzakowalska <zuzanna@kowalska.pro>
2017-08-04 09:54:55 +0200

    required checkbox validation done

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,31 @@
 'use strict';
 
-function checkTextField() {
+function validateFields() {
 
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
+    var agree = document.getElementById("agree-1");
     var nameRequired = document.getElementById("name-required");
     var emailRequired = document.getElementById("email-required");
+    var checkRequired = document.getElementById("check-required");
 
     if (name.length == 0) {
         event.preventDefault();
         console.log("no name");
         nameRequired.style.opacity = "1";
     } else {
-        nameRequired.style.opacity = "0";
-        console.log(name);
-    }
+        nameRequired.style.opacity = "0"; }
 
     if (email.length == 0) {
         event.preventDefault();
         console.log("no email");
         emailRequired.style.opacity = "1";
     } else {
-        nameRequired.style.opacity = "0";
-        console.log(email);
-    }
+        emailRequired.style.opacity = "0"; }
+    
+    if (!agree.checked) {
+        event.preventDefault();
+        checkRequired.style.opacity = "1";
+    } else {
+    checkRequired.style.opacity = "0"; }
 }
